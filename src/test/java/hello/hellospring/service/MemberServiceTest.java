f95e08c705540eb860fb3b316fd6467f9a458556
Dahun Jeong <gnsgnsek@gmail.com>
2021-06-01 22:57:30 +0900

    Create Sample Project (#1)
    * feat: Create Member Domain
    
    * feat: Create MemberRepositry, MemmoryMemberRepositroy
    
    * refactor: Modify MemberRepository findByMember to findByName
    
    * feat: Create MemoryMemberRepository clear memory feature
    
    * test: Create MemoryMemberRespository test
    
    * feat: Create MemberService feat
    
    * test: Create MemberService test

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @Test
+    void joinTest() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void duplicateMemberException() {
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
