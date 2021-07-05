3b3141d506f2249dda5aec22d02d2c50356e5c2a
PandaHun <gnsgnsek@gmail.com>
2021-07-05 22:07:36 +0900

    feat: Create JDBC Template version project

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,12 +21,15 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
-    public String createForm(MemberForm form) {
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
-
         memberService.join(member);
-
         return "redirect:/";
     }
 
